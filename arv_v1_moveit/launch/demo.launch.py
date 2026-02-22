f9910748ea86d815e9525c0256f5954425c25c56
Young-Yihang <3119801747@qq.com>
2026-02-23 02:23:41 +0800

    enh: regenerate

--- arv_v1_moveit/launch/demo.launch.py
+++ arv_v1_moveit/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("arv_v1_model", package_name="arv_v1_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("arv_v1", package_name="arv_v1_moveit").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- arv_v1_moveit/launch/static_virtual_joint_tfs.launch.py
+++ arv_v1_moveit/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("arv_v1_model", package_name="arv_v1_moveit").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("arv_v1", package_name="arv_v1_moveit").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
